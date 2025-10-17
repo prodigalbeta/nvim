8e54bab0e0c7e3b9cbf2a4e1ea291b15ed53b75e
prodigalbeta <137588668+prodigalbeta@users.noreply.github.com>
2025-10-17 13:06:55 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ vim.wo.relativenumber = true
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
-vim.cmd("shiftwidth=2")
+vim.cmd("set shiftwidth=2")
 vim.cmd("set conceallevel=2")
 
 require("config.lazy")
